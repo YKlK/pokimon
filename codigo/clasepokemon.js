1b5d7084577939248ace1cc37813142867d6de77
Pedro Jose Manzueta Sepulveda <pedromanzueta017@gmail.com>
2022-04-22 18:59:18 -0400

    haciendo la clase

--- codigo/clasepokemon.js
+++ codigo/clasepokemon.js
@@ -1,3 +1,74 @@
 class Pokemon{
-    
+
+    constructor(){
+    let vida;
+    let ataque;
+    // let ataqueEspecial; heredable
+    // let defensaEspecia; heredable
+    let defensa;
+    let velocidad;
+    let estadoVital;
+    }
+
+
+    /*funciones getters y setters */
+    get vida(){
+        return this.vida
+    }
+
+    set vida(vida){
+        this.vida = vida
+    }
+
+    get ataque(){
+        return this.ataque
+    }
+
+    set ataque(ataque){
+        this.ataque = ataque
+    }
+
+    get defensa(){
+        return this.defensa
+    }
+
+    set defensa(defensa){
+        this.defensa = defensa
+    }
+
+    get velocidad(){
+        return this.velocidad;  
+    }
+
+    set velocidad(velocidad){
+        this.velocidad = velocidad
+    }
+
+    get estadoVital(){
+        return this.estadoVital
+    }
+
+    set estadoVital(estadoVital){
+        this.estadoVital = estadoVital
+    }
+
+    /*funciones getters y setters */
+
+   atacar(){
+
+   }
+
+
+   PerderVida(){
+
+   }
+
+
+   defenderse(){
+
+   }
+
+   estadoDeVida(){
+
+   }
 }
